31c3d5a51ad37affd15c70995771d5a3631aa61f
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2024-07-26 12:54:42 -0400

    Initial commit

---
+++ src/main/java/com/biojj/helpregistration/HelpRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.biojj.helpregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpRegistrationApplication.class, args);
+	}
+
+}
