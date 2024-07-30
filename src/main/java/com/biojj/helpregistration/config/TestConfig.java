7199fcde1e3c435ba7a8110b8325a6db6db2c18d
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2024-07-30 08:37:24 -0400

    feat: 💫 add config and security

--- src/main/java/com/biojj/helpregistration/config/TestConfig.java
+++ src/main/java/com/biojj/helpregistration/config/TestConfig.java
@@ -1,2 +1,22 @@
-package com.biojj.helpregistration.config;public class TestConfig {
+package com.biojj.helpregistration.config;
+
+import com.biojj.helpregistration.service.DBService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    private final DBService dbService;
+
+    public TestConfig(DBService dbService) {
+        this.dbService = dbService;
+    }
+
+    @PostConstruct
+    public void init() {
+        dbService.InstanciaDB();
+    }
 }
--- src/main/java/com/biojj/helpregistration/security/UserSS.java
+++ src/main/java/com/biojj/helpregistration/security/UserSS.java
@@ -1,2 +1,77 @@
-package com.biojj.helpregistration.security;public class UserSS {
+package com.biojj.helpregistration.security;
+
+import com.biojj.helpregistration.domain.enums.Profile;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserSS implements UserDetails {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Getter
+    private Integer id;
+    private String email;
+    private String password;
+    @Getter
+    private String name;
+    @Getter
+    private String cpf;
+    private Collection<? extends GrantedAuthority> authorities;
+
+
+    public UserSS(Integer id, String email, String password, Set<Profile> perfis, String name, String cpf) {
+        super();
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toSet());
+        this.name = name;
+        this.cpf = cpf;
+    }
+
+    public UserSS() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
